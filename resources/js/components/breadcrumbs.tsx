de46fb2a5f00762d8a51429c0674bf87d1323cda
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-21 13:56:38 -0400

    feat(breadcrumbs): refactor Breadcrumbs component to display current team with avatar and dropdown menu for better user experience

--- resources/js/components/breadcrumbs.tsx
+++ resources/js/components/breadcrumbs.tsx
@@ -1,34 +1,39 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
-import { type BreadcrumbItem as BreadcrumbItemType } from "@/types"
-import { Link } from "@inertiajs/react"
-import { Fragment } from "react"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from "@/components/ui/breadcrumb"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { useInitials } from "@/hooks/use-initials"
+import { cn } from "@/lib/utils"
+import { type SharedData } from "@/types"
+import { Link, usePage } from "@inertiajs/react"
+import { ChevronsUpDownIcon } from "lucide-react"
 
-export function Breadcrumbs({ breadcrumbs }: { breadcrumbs: BreadcrumbItemType[] }) {
+export function Breadcrumbs() {
+  const getInitials = useInitials()
+  const { currentTeam } = usePage<SharedData>().props
   return (
-    <>
-      {breadcrumbs.length > 0 && (
-        <Breadcrumb>
-          <BreadcrumbList>
-            {breadcrumbs.map((item, index) => {
-              const isLast = index === breadcrumbs.length - 1
-              return (
-                <Fragment key={index}>
-                  <BreadcrumbItem>
-                    {isLast ? (
-                      <BreadcrumbPage>{item.title}</BreadcrumbPage>
-                    ) : (
-                      <BreadcrumbLink asChild>
-                        <Link href={item.href}>{item.title}</Link>
-                      </BreadcrumbLink>
-                    )}
-                  </BreadcrumbItem>
-                  {!isLast && <BreadcrumbSeparator />}
-                </Fragment>
-              )
-            })}
-          </BreadcrumbList>
-        </Breadcrumb>
-      )}
-    </>
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href={route("team.overview", [currentTeam])} className={cn(buttonVariants({ variant: "ghost" }), "px-2 py-1")}>
+              <Avatar className={"size-6 rounded-md"}>
+                <AvatarImage src={currentTeam?.logo} />
+                <AvatarFallback className={"rounded-md text-xs font-black"}>{getInitials(currentTeam?.name ?? "")}</AvatarFallback>
+              </Avatar>
+              {currentTeam?.name}
+            </Link>
+          </BreadcrumbLink>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant={"ghost"} size={"icon"} className={"w-6"}>
+                <ChevronsUpDownIcon />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align={"end"}></DropdownMenuContent>
+          </DropdownMenu>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
   )
 }
