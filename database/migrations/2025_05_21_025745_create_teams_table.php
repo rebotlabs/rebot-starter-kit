8369e70351227a1fe17502691972c70a4e30eb6c
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-21 12:03:39 -0400

    feat(migration): create teams table and add current_team_id to users table for team association

--- database/migrations/2025_05_21_025745_create_teams_table.php
+++ database/migrations/2025_05_21_025745_create_teams_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('teams', function (Blueprint $table) {
@@ -16,10 +15,17 @@ return new class extends Migration
             $table->foreignId('owner_id')->constrained('users');
             $table->timestamps();
         });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('current_team_id')->after('id')->nullable()->constrained('teams')->nullOnDelete();
+        });
     }
 
     public function down(): void
     {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('current_team_id');
+        });
         Schema::dropIfExists('teams');
     }
 };
