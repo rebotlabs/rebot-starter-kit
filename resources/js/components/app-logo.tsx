cc40d77e6f4385169036fb53c5235f6aa2fa5c78
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-20 22:05:51 -0400

    feat: remove app name beside logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -6,9 +6,6 @@ export default function AppLogo() {
       <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
         <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
       </div>
-      <div className="ml-1 grid flex-1 text-left text-sm">
-        <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
-      </div>
     </>
   )
 }
