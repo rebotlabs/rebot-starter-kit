86a93cc3acf1b857790fda26af67ccbdd4651640
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-21 19:57:44 -0400

    style: apply code style improvements for consistency across multiple files, including spacing and formatting adjustments in HandleInertiaRequests.php, app.php, teams migration, and web.php

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -42,13 +42,13 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
-            'ziggy' => fn(): array => [
+            'ziggy' => fn (): array => [
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
-            'sidebarOpen' => !$request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
+            'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
             'currentTeam' => $request->user()?->currentTeam,
-//          'teams' => $request->user()?->teams()->get(),
+            //          'teams' => $request->user()?->teams()->get(),
         ];
     }
 }
