071afdd841fb02e92d2ca1eb7aeeeb1dbad164b8
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-21 06:59:13 -0400

    feat: add team with billable

---
+++ app/Models/Team.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\TeamFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Laravel\Cashier\Billable;
+
+class Team extends Model
+{
+    /** @use HasFactory<TeamFactory> */
+    use Billable, HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'logo',
+        'owner_id',
+    ];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+}
---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Team>
+ */
+class TeamFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->unique()->company(),
+            'slug' => fake()->slug(2),
+            'logo' => fake()->imageUrl(128, 128, 'business'),
+            'owner_id' => User::factory(),
+        ];
+    }
+}
