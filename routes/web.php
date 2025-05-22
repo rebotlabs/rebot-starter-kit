a8b8af69cadb9af23f97b2269ae56c5a431f57aa
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-21 22:19:20 -0400

    style(web.php): remove unnecessary spaces in require statements for consistency

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('team.settings.billing');
 });
 
-require __DIR__ . '/settings.php';
-require __DIR__ . '/auth.php';
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
