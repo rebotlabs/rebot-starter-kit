c53198d5773ec8250c4a8296e81c79056dd7844a
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-23 11:35:21 -0400

    feat(MembersController.php): enhance member invitation functionality by adding invitation tokens and user ID retrieval for invited members

--- app/Http/Controllers/Team/Settings/MembersController.php
+++ app/Http/Controllers/Team/Settings/MembersController.php
@@ -6,21 +6,33 @@ namespace App\Http\Controllers\Team\Settings;
 
 use App\Http\Controllers\Controller;
 use App\Models\Team;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class MembersController extends Controller
 {
     public function show(Request $request, Team $team)
     {
-        return Inertia::render('team/settings/members');
+        return Inertia::render('team/settings/members', [
+            'invitations' => fn() => $team->invitations()->get(),
+        ]);
     }
 
     public function invite(Request $request, Team $team)
     {
-        $request->validate([
+        $data = $request->validate([
             'email' => ['required', 'email'],
             'role' => ['required', 'string', 'in:admin,member'],
+        ]);
+
+        $team->invitations()->create([
+            'email' => $data['email'],
+            'role' => $data['role'],
+            'user_id' => User::query()->where('email', $data['email'])->first()?->id,
+            'accept_token' => Str::random(32),
+            'reject_token' => Str::random(32),
         ]);
     }
 }
