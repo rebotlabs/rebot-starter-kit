5841d60ce09b99c55e9fa0eb9885687b162190be
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-23 11:01:56 -0400

    feat(web.php): refactor team settings routes to use controllers for better organization and maintainability

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Team\Settings\GeneralController;
+use App\Http\Controllers\Team\Settings\MembersController;
 use App\Models\Team;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,18 +20,16 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('team/overview');
     })->name('team.overview');
 
-    Route::get('team/{team}/settings', function (Team $team) {
-        return Inertia::render('team/settings/general');
-    })->name('team.settings');
+    Route::get('team/{team}/settings', [GeneralController::class, "show"])->name('team.settings');
+    Route::patch('team/{team}/settings', [GeneralController::class, "update"])->name('team.settings.update');
 
-    Route::get('team/{team}/settings/members', function (Team $team) {
-        return Inertia::render('team/settings/members');
-    })->name('team.settings.members');
+    Route::get('team/{team}/settings/members', [MembersController::class, "show"])->name('team.settings.members');
+    Route::post('team/{team}/settings/members/invite', [MembersController::class, "invite"])->name('team.settings.members.invite');
 
     Route::get('team/{team}/settings/billing', function (Team $team) {
         return Inertia::render('team/settings/billing');
     })->name('team.settings.billing');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
