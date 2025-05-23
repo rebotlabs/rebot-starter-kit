5dbf56fb2a0879ae3973871dd414235115b6ddc7
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-23 11:18:35 -0400

    feat(Invitation): create Invitation model to manage team invitations and relationships with users and teams

---
+++ app/Models/Invitation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\InvitationFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Invitation extends Model
+{
+    /** @use HasFactory<InvitationFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'team_id',
+        'user_id',
+        'email',
+        'role',
+        'accept_token',
+        'reject_token',
+        'status',
+    ];
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
