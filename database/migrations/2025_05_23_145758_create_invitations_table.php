aed9be682c8f240683c99d14ee6d74271efbf651
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-23 11:18:08 -0400

    feat(migrations): create invitations table for managing team invitations and user roles

---
+++ database/migrations/2025_05_23_145758_create_invitations_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('email')->index();
+            $table->string('role')->default('member');
+            $table->string('accept_token')->unique();
+            $table->string('reject_token')->unique();
+            $table->string('status')->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('invitations');
+    }
+};
