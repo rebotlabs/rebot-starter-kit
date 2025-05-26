35a28ca40dad419ce9b82fe9e0390991904773cc
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-26 10:06:44 -0400

    feat(MemberFactory): create a factory for generating Member model instances to streamline testing and seeding processes

---
+++ database/factories/MemberFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Member;
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Member>
+ */
+class MemberFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'team_id' => Team::factory(),
+            'user_id' => User::factory(),
+            'role' => fake()->randomElement(['admin', 'member']),
+        ];
+    }
+}
