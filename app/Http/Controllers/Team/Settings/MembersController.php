a2a195999fc4ed1dfa0c7e2a056e9c8d460a58a8
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-26 14:07:17 -0400

    feat(MembersController.php): implement invitation notification and add resend and delete methods for managing team invitations

--- app/Http/Controllers/Team/Settings/MembersController.php
+++ app/Http/Controllers/Team/Settings/MembersController.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Team\Settings;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invitation;
 use App\Models\Team;
 use App\Models\User;
+use App\Notifications\InvitationSent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Inertia\Inertia;
@@ -28,12 +30,24 @@ class MembersController extends Controller
             'role' => ['required', 'string', 'in:admin,member'],
         ]);
 
-        $team->invitations()->create([
+        $invitation = $team->invitations()->create([
             'email' => $data['email'],
             'role' => $data['role'],
             'user_id' => User::query()->where('email', $data['email'])->first()?->id,
             'accept_token' => Str::random(32),
             'reject_token' => Str::random(32),
         ]);
+
+        $invitation->notify(new InvitationSent());
+    }
+
+    public function resend(Request $request, Team $team, Invitation $invitation)
+    {
+        $invitation->notify(new InvitationSent());
+    }
+
+    public function delete(Request $request, Team $team, Invitation $invitation)
+    {
+        $invitation->delete();
     }
 }
