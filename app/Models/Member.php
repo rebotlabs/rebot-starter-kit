335c2b5e3989609a45e711c1239cff888367c44c
Vincent Talbot <vincent.talbot@gmail.com>
2025-05-26 10:07:01 -0400

    feat(Member.php): create Member model to represent team members and their relationships with teams and users

---
+++ app/Models/Member.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\MemberFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Member extends Model
+{
+    /** @use HasFactory<MemberFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'team_id',
+        'user_id',
+        'role',
+    ];
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
+    
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
